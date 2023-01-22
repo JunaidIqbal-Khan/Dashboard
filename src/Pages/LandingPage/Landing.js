bc050e7e7835da754863857d24af057087911bcd
faseeha batool <faseehabatool5@gmail.com>
2023-01-22 19:55:00 +0500

    slider images issue

--- src/Pages/LandingPage/Landing.js
+++ src/Pages/LandingPage/Landing.js
@@ -1,8 +1,39 @@
 import React from 'react'
 import "./Landing.css"
+import img1 from "../../images/img1.jpg"
+import img2 from "../../images/img2.jpg"
+import img3 from "../../images/img3.jpg"
+import img4 from "../../images/img4.jpg"
+
 function Landing() {
   return (
-    <div>Landing page </div>
+    <div onload="slider()">
+    <div class="banner">
+            <div class="slidder">
+              <img src="img1.jpg" id="slideImg"/>
+            </div>
+            <div class="overlay">
+              <div class="navbar">
+                <div class="logo">
+                  <h1>Logo</h1>
+                </div>
+                <div class="menu-icon">
+                   <img src="img1.jpg" id="slideImg"/>
+                  <img src="img2.jpg" id="slideImg"/>
+                  <img src="img3.jpg" id="slideImg"/> 
+                </div>
+              </div>
+              <div class="content">
+                <h1>LET'S START TO DESIGN</h1>
+                <h3>LET'S START TO DESIGN</h3>
+                <div>
+                  <button type="button">SignUp</button>
+                  <button type="button" class="btn-2">Login/SignIn</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   )
 }
 
