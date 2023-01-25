a0e78d37351c61d18d15ebc794a4d0c7f1195767
faseeha batool <faseehabatool5@gmail.com>
2023-01-25 19:25:06 +0500

    sign in complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import Landing from './Pages/LandingPage/Landing';
+import SignIn from './Pages/SignInPage/SignIn';
+
 function App() {
   return (
     <div className="App">
-     <Landing/>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/SignIn" element={<SignIn/>}/>
+
+      </Routes>
     </div>
   );
 }
--- src/Pages/LandingPage/Landing.js
+++ src/Pages/LandingPage/Landing.js
@@ -1,40 +1,93 @@
 import React from 'react'
 import "./Landing.css"
+import { useEffect, useState } from 'react'
+import SignIn from '../SignInPage/SignIn'
 import img1 from "../../images/img1.jpg"
 import img2 from "../../images/img2.jpg"
 import img3 from "../../images/img3.jpg"
 import img4 from "../../images/img4.jpg"
-
+import {NavLink} from 'react-router-dom'
 function Landing() {
+
+const [isSignIn,setIsSignIn] = useState(false);
+
+
+
+
+//   const slidercall=(len,i,Images,slideImg)=>{
+//     if (i > len-1){
+//       i=0;
+//     }
+//     slideImg.src = Images[i];
+//     i++;
+//     setTimeout(slidercall(),3000);
+//   }
+//   useEffect(() => {
+   
+    
+//       var slideImg = document.getElementById("slideImg");
+// var Images = new Array(
+//   img1,
+//   img2,
+//   img3,
+//   img4,
+// );
+
+// var len = Images.length;
+// var i=0;
+
+//     }
+//   , []);
+
   return (
+    <>
     <div onload="slider()">
     <div class="banner">
             <div class="slidder">
-              <img src="img1.jpg" id="slideImg"/>
+              <img src={img1} id="slideImg"/>
             </div>
             <div class="overlay">
               <div class="navbar">
                 <div class="logo">
                   <h1>Logo</h1>
                 </div>
-                <div class="menu-icon">
-                   <img src="img1.jpg" id="slideImg"/>
-                  <img src="img2.jpg" id="slideImg"/>
-                  <img src="img3.jpg" id="slideImg"/> 
-                </div>
+             
               </div>
               <div class="content">
                 <h1>LET'S START TO DESIGN</h1>
                 <h3>LET'S START TO DESIGN</h3>
                 <div>
-                  <button type="button">SignUp</button>
-                  <button type="button" class="btn-2">Login/SignIn</button>
+                  <button type="button" onClick={()=>setIsSignIn(true)}>Let's Start</button>
+                  {/* <button type="button" class="btn-2">Login/SignIn</button> */}
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <SignIn open={isSignIn} setOpen={setIsSignIn}/>
+        </>
   )
 }
 
 export default Landing
+{/* <script>
+var slideImg = document.getElementById("slideImg");
+var Images = new Array(
+  "Images/img1.jpg",
+  "Images/img2.jpg",
+  "Images/img3.jpg",
+  "Images/img4.jpg"
+);
+
+var len = Images.length;
+var i=0;
+function slider (){
+  if (i > len-1){
+    i=0;
+  }
+  slideImg.src = Images[i];
+  i++;
+  setTimeout('slider ()',3000);
+
+}
+</script> */}
