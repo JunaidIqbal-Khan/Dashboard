29f6a0ad76e98879a51d941f19760dbb0e832935
Junaid Iqbal <jk601677@gmail.com>
2023-02-06 16:30:23 +0500

    feedback divs

--- src/Pages/feedBack/FeedBack.jsx
+++ src/Pages/feedBack/FeedBack.jsx
@@ -7,40 +7,50 @@ function FeedBack() {
     <div className="feedback">
     <span className="feedBackTitle">Feedbacks</span>
     <ul className="feedBackList">
-      <li className="feedBackListItem">
+    <li className="feedBackListItem">
+        <div>
         <img
           src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
           alt=""
           className="feedBackImg"
         />
-        <div className="widgetSmUser">
+         <div className="feedbackUser">
           <span className="Username">Anna Keller</span>
-          <span className="widgetSmUserTitle">Software Engineer</span>
+          <span className="feedbackDate">Date</span>
+        </div>
+        </div>
+       <div className='paragraph'>
+        <p>n this video, we will learn how to update & display user profile image using Class Component in MERN Fullstack Project from Reactjs Frontend using Nodejs</p>
         </div>
         <div>
-        <button className="widgetSmButton">
-          <Visibility className="widgetSmIcon" />
-          Display User
+        <button className="feedbackButton">
+          Respond
         </button>
         </div>
       </li>
-
       <li className="feedBackListItem">
+        <div>
         <img
           src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
           alt=""
           className="feedBackImg"
         />
-        <div className="widgetSmUser">
+         <div className="feedbackUser">
           <span className="Username">Anna Keller</span>
-          <span className="widgetSmUserTitle">Software Engineer</span>
+          <span className="feedbackDate">Date</span>
         </div>
-        <button className="widgetSmButton">
-          <Visibility className="widgetSmIcon" />
-          Display User
+        </div>
+       <div className='paragraph'>
+        <p>n this video, we will learn how to update & display user profile image using Class Component in MERN Fullstack Project from Reactjs Frontend using Nodejs</p>
+        </div>
+        <div>
+        <button className="feedbackButton">
+         
+          Respond
         </button>
+        </div>
+        
       </li>
-
 
       
      
